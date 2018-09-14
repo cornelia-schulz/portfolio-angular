4b6caa1a4dc4a75902fbdfce6ef536fd265a14ef
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-09-14 21:56:39 +1200

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlurbComponent } from './blurb/blurb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    ProjectsComponent,
+    ContactComponent,
+    BlurbComponent
   ],
   imports: [
     BrowserModule
