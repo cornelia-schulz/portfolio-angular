f1be0aa748231443042da5e46c0f46a96f6ee881
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-09-17 18:52:00 +1200

    added send button to contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppMaterialModule } from './app-material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
@@ -24,7 +26,9 @@ import { BlurbComponent } from './blurb/blurb.component';
   imports: [
     BrowserModule,
     AppMaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
