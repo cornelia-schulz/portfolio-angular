f2031fdfb586f925b8bb4d0d8af457d205cf420e
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-09-18 19:35:15 +1200

    added responsiveness to banner and projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
   title='Some of my projects'
+  breakpoint: number
   constructor() { }
 
   ngOnInit() {
+    this.breakpoint = (window.innerWidth <= 500) ? 1 : 2;
+  }
+
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 500) ? 1 : 2;
   }
 
   showText(e){
