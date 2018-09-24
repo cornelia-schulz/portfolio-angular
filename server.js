b6aa03f1c7869c26f57bd5bb864b7d60f00856aa
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-09-24 18:27:49 +1200

    added express server

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-// const path = require('path')
+const path = require('path')
 const express = require('express')
-const server = express()
+const http = require('http')
+const app = express()
 const bodyParser = require('body-parser')
-server.use(bodyParser.json())
+
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, '/dist/portfolio-angular')))
 
 const port = process.env.PORT || 3000
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname))
+})
+
+const server = http.createServer(app)
 
 server.listen(port, () => {
   console.log('Server is listening on port: ', port)
 })
 
-server.route('/api/portfolio').get((req, res) => {
-  res.send(
-    'hello'
-  )
-})
-
-module.exports = server
+module.exports = app
