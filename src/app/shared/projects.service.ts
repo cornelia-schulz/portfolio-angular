4656068a911d156e943102c8138da008eaf3e4fe
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2020-05-26 19:49:38 +1000

    add more projects

--- src/app/shared/projects.service.ts
+++ src/app/shared/projects.service.ts
@@ -38,6 +38,42 @@ export class ProjectsService {
       github: 'https://github.com/cornelia-schulz/cornelia-schulz-photography',
       website: 'http://www.cornelia-schulz-photography.com',
       image: '../../../assets/images/cornelia-schulz-photography-responsive.jpg?v=1'
+    },
+    {
+      id: 5,
+      name: 'Cornelia Schulz Photography React',
+      description: 'A new version of my photography portfolio.',
+      technologies: ['HTML', 'SCSS', 'JavaScript', 'React', 'Jest', 'Knex', 'Express'],
+      github: 'https://github.com/cornelia-schulz/photography',
+      website: 'https://cornelia-photography.herokuapp.com/',
+      image: '../../../assets/images/cornelia-schulz-photography-react-responsive.jpg?v=1'
+    },
+    {
+      id: 6,
+      name: 'Where in the world',
+      description: 'A coding challenge from Frontend Mentor.',
+      technologies: ['HTML', 'CSS', 'VueJs', 'Axios', 'SCSS'],
+      github: 'https://github.com/cornelia-schulz/fm-rest-countries-api-with-color-theme-switcher',
+      website: 'https://frosty-albattani-ec5ec1.netlify.app/',
+      image: '../../../assets/images/where-in-the-world-responsive.jpg?v=1'
+    },
+    {
+      id: 7,
+      name: 'URL Shortening',
+      description: 'A coding challenge from Frontend Mentor.',
+      technologies: ['React', 'HTML', 'CSS', 'JavaScript', 'Axios', 'Typescript'],
+      github: 'https://github.com/cornelia-schulz/fm-url-shortening-api',
+      website: 'https://objective-einstein-222a85.netlify.app/',
+      image: '../../../assets/images/url-shortening-responsive.jpg?v=1'
+    },
+    {
+      id: 8,
+      name: 'Pricing Component',
+      description: 'A coding challenge from Frontend Mentor.',
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+      github: 'https://github.com/cornelia-schulz/fm-pricing-component-with-toggle',
+      website: 'https://cornelia-schulz.github.io/fm-pricing-component-with-toggle/',
+      image: '../../../assets/images/pricing-component-responsive.jpg?v=1'
     }
   ];
 }
